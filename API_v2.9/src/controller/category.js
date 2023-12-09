99950ab21c2b97ab44c119614f1462781a4ca3b4
Cédric M <128589682+martcHenallux@users.noreply.github.com>
2023-12-09 15:15:18 +0100

    Add files via upload

---
+++ API_v2.9/src/controller/category.js
@@ -0,0 +1,335 @@
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      Category:
+ *          type: object
+ *          properties:
+ *              genre_id:
+ *                  type: integer
+ *                  description: The genre id
+ *              video_game_id:
+ *                  type: integer
+ *                  description: The video game id
+ */
+
+const pool = require("../model/database");
+const CategoryModel = require("../model/category");
+const HTTPStatus = require("../tools/HTTPStatus");
+const PGErrors = require("../tools/PGErrors");
+const { validateObject } = require("../zod/zod");
+const {
+    categoryIdsSchema,
+    categorySchema,
+    getCategorySchema,
+} = require("../zod/schema/category");
+
+/**
+ * Create a category
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ *
+ * @swagger
+ * components:
+ *  responses:
+ *      CategoryAdded:
+ *          description: The category was added
+ *  requestBodies:
+ *      CategoryToAdd:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          genre_id:
+ *                              type: integer
+ *                              description: The genre id
+ *                          video_game_id:
+ *                              type: integer
+ *                              description: The video game id
+ *                      required:
+ *                          - genre_id
+ *                          - video_game_id
+ */
+module.exports.createCategory = async (req, res) => {
+    let category;
+    try {
+        category = validateObject(req.body, categorySchema);
+    } catch (error) {
+        res.status(HTTPStatus.BAD_REQUEST).json({
+            code: "INVALID_INPUT",
+            message: error.message,
+        });
+        return;
+    }
+    const { genre_id: genreId, video_game_id: videoGameId } = category;
+
+    const client = await pool.connect();
+    try {
+        await CategoryModel.createCategory(client, genreId, videoGameId);
+        res.sendStatus(HTTPStatus.CREATED);
+    } catch (error) {
+        switch (error.code) {
+            case PGErrors.UNIQUE_VIOLATION:
+                res.status(HTTPStatus.CONFLICT).json({
+                    code: "DUPLICATE_ENTRY",
+                    message: error.detail,
+                });
+                break;
+            case PGErrors.FOREIGN_KEY_VIOLATION:
+                res.status(HTTPStatus.NOT_FOUND).json({
+                    code: "FOREIGN_KEY_NOT_FOUND",
+                    message: error.detail.replace(/"/g, ""),
+                });
+                break;
+            default:
+                res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR);
+        }
+    } finally {
+        client.release();
+    }
+};
+
+/**
+ * Get one or more categories
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ *
+ * @swagger
+ * components:
+ *  responses:
+ *      CategoryFound:
+ *          description: Category(ies) was(were) found
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: array
+ *                      items:
+ *                          $ref: '#/components/schemas/Category'
+ */
+module.exports.getCategory = async (req, res) => {
+    let genreId, videoGameId, page, limit;
+    try {
+        ({ genreId, videoGameId, page, limit } = validateObject(
+            req.query,
+            getCategorySchema
+        ));
+    } catch (error) {
+        res.status(HTTPStatus.BAD_REQUEST).json({
+            code: "INVALID_INPUT",
+            message: error.message,
+        });
+        return;
+    }
+
+    const client = await pool.connect();
+    try {
+        const { rows: categories } = await CategoryModel.getCategories(
+            client,
+            genreId,
+            videoGameId,
+            page,
+            limit
+        );
+        if (categories.length === 0) {
+            res.status(HTTPStatus.NOT_FOUND).json({
+                code: "RESOURCE_NOT_FOUND",
+                message: "No category found",
+            });
+            return;
+        }
+        res.json(categories);
+    } catch (error) {
+        res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR);
+    } finally {
+        client.release();
+    }
+};
+
+/**
+ * Get the number of categories
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ *
+ * @swagger
+ * components:
+ *  responses:
+ *      CategoryCount:
+ *          description: The number of categories
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: integer
+ *                      properties:
+ *                          no:
+ *                              type: integer
+ *                              description: The number of categories
+ */
+module.exports.getCategoriesCount = async (req, res) => {
+    const client = await pool.connect();
+    try {
+        const { rows: categories } =
+            await CategoryModel.getCategoriesCount(client);
+        if (categories.length === 0) {
+            res.status(HTTPStatus.NOT_FOUND).json({
+                code: "RESOURCE_NOT_FOUND",
+                message: "No category found",
+            });
+            return;
+        }
+        res.json(categories[0].no);
+    } catch (error) {
+        res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR);
+    } finally {
+        client.release();
+    }
+};
+
+/**
+ * Update a category
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ *
+ * @swagger
+ * components:
+ *  responses:
+ *      CategoryUpdated:
+ *          description: The category was updated
+ *  requestBodies:
+ *      CategoryToUpdate:
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          genre_id:
+ *                              type: integer
+ *                              description: The genre id
+ *                          video_game_id:
+ *                              type: integer
+ *                              description: The video game id
+ */
+module.exports.updateCategory = async (req, res) => {
+    let genreId, videoGameId, updateValues;
+    try {
+        ({ genreId, videoGameId } = validateObject(
+            req.params,
+            categoryIdsSchema
+        ));
+        updateValues = validateObject(req.body, categorySchema.partial());
+    } catch (error) {
+        res.status(HTTPStatus.BAD_REQUEST).json({
+            message: error.message,
+        });
+        return;
+    }
+
+    if (Object.keys(updateValues).length === 0) {
+        res.status(HTTPStatus.BAD_REQUEST).json({
+            code: "INVALID_INPUT",
+            message: "No value to update",
+        });
+        return;
+    }
+
+    const client = await pool.connect();
+
+    try {
+        const { rowCount } = await CategoryModel.updateCategory(
+            client,
+            genreId,
+            videoGameId,
+            updateValues
+        );
+        if (rowCount === 0) {
+            res.status(HTTPStatus.NOT_FOUND).json({
+                code: "RESOURCE_NOT_FOUND",
+                message: "No category found",
+            });
+            return;
+        }
+        res.sendStatus(HTTPStatus.NO_CONTENT);
+    } catch (error) {
+        switch (error.code) {
+            case PGErrors.FOREIGN_KEY_VIOLATION:
+                res.status(HTTPStatus.NOT_FOUND).json({
+                    code: "FOREIGN_KEY_NOT_FOUND",
+                    message: error.detail.replace(/"/g, ""),
+                });
+                break;
+            case PGErrors.UNIQUE_VIOLATION:
+                res.status(HTTPStatus.CONFLICT).json({
+                    code: "DUPLICATE_ENTRY",
+                    message: error.detail,
+                });
+                break;
+            default:
+                res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR);
+        }
+    } finally {
+        client.release();
+    }
+};
+
+/**
+ * Delete a category
+ *
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * @returns {Promise<void>}
+ *
+ * @swagger
+ * components:
+ *  responses:
+ *      CategoryDeleted:
+ *          description: The category was deleted
+ */
+module.exports.deleteCategory = async (req, res) => {
+    let genreId, videoGameId;
+    try {
+        ({ genreId, videoGameId } = validateObject(
+            req.params,
+            categoryIdsSchema
+        ));
+    } catch (error) {
+        res.status(HTTPStatus.BAD_REQUEST).json({
+            code: "INVALID_INPUT",
+            message: error.message,
+        });
+        return;
+    }
+
+    const client = await pool.connect();
+    try {
+        const { rowCount } = await CategoryModel.deleteCategory(
+            client,
+            genreId,
+            videoGameId
+        );
+        if (rowCount === 0) {
+            res.status(HTTPStatus.NOT_FOUND).json({
+                code: "RESOURCE_NOT_FOUND",
+                message: "No category found",
+            });
+            return;
+        }
+        res.sendStatus(HTTPStatus.NO_CONTENT);
+    } catch (error) {
+        res.sendStatus(HTTPStatus.INTERNAL_SERVER_ERROR);
+    } finally {
+        client.release();
+    }
+};
---
+++ API_v2.9/src/zod/schema/publication.js
@@ -0,0 +1,34 @@
+const { z } = require("zod");
+
+module.exports.publicationIdSchema = z.object({
+    publicationId: z.coerce.number().int(),
+});
+
+module.exports.publicationSchema = z.object({
+    platform_code: z.string().trim(),
+    video_game_id: z.coerce.number().int(),
+    release_date: z.coerce.date(),
+    release_price: z.coerce.number().optional().nullable(),
+    store_page_url: z.string().trim().url().optional().nullable(),
+});
+
+module.exports.publicationToGetSchema = z.object({
+    publicationId: z.coerce.number().int().optional(),
+    videoGameId: z.coerce.number().optional(),
+    videoGameName: z.string().optional(),
+    platformCode: z.string().toUpperCase().optional(),
+    getOwnGames: z
+        .enum(["true", "false", "True", "False"])
+        .transform((value) => value.toLowerCase() === "true")
+        .optional(),
+    getLastGames: z
+        .enum(["true", "false", "True", "False"])
+        .transform((value) => value.toLowerCase() === "true")
+        .optional(),
+    alphabetical: z
+        .enum(["true", "false", "True", "False"])
+        .transform((value) => value.toLowerCase() === "true")
+        .optional(),
+    page: z.coerce.number().int().min(1).optional(),
+    limit: z.coerce.number().int().min(1).max(50).optional(),
+});
