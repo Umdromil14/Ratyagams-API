6bbf6ffaa5d4fa50d5eaaa6dc9219ec0878488e9
Baras Cyril <121720124+Umdromil14@users.noreply.github.com>
2023-12-21 18:53:48 +0100

    Delete API_v2.5 directory

--- API_v2.5/src/zod/schema/pagination.js
+++
@@ -1,6 +0,0 @@
-const { z } = require('zod');
-
-module.exports.paginationSchema = z.object({
-    page : z.coerce.number().int().positive().optional().default(1),
-    limit : z.coerce.number().int().positive().optional().default(10),
-});
